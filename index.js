a8ac93730bf05fe89ceed55b06067b657cbb8b08
Maxime Genaudet <maxime.genaudet@soprabanking.com>
2018-10-23 14:18:31 +0200

    change default listening port

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
 
 io.on('connection', function(socket){
