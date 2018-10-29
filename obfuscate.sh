dc406997001af8b6732231401fa588931396f424
MikeLitoris <maxime.genaudet@gmail.com>
2018-10-29 22:19:40 +0100

    script obfuscation

---
+++ obfuscate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+javascript-obfuscator public/js/entities/entities.js --output  public/js/entities/entities.js
+javascript-obfuscator public/js/entities/HUD.js --output  public/js/entities/HUD.js
+javascript-obfuscator public/js/screens/gameover.js --output  public/js/screens/gameover.js
+javascript-obfuscator public/js/screens/play.js --output  public/js/screens/play.js
+javascript-obfuscator public/js/screens/title.js --output  public/js/screens/title.js
+javascript-obfuscator public/js/game.js --output  public/js/game.js
