de05025446610630f433db114463ec6c665b614f
MikeLitoris <maxime.genaudet@gmail.com>
2018-12-09 17:29:10 +0100

    pffff

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM node:8.12.0-alpine
 
 COPY . /flappysanta/
 
-ENV PORT=8080
-
 USER 0
 
 RUN chmod +x flappysanta/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sed -i 's|8080|'"$PORT"'|g' flappysanta/index.js
 cd flappysanta && npm install && npm link javascript-obfuscator && javascript-obfuscator ./public/js/ --output ./public/js/ && node index.js > flappysanta.log
