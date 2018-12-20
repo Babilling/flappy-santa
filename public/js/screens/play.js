24959109875c8036a53c07b5d1feaa2d4e92f4cb
Maxime Genaudet <maxime.genaudet@soprabanking.com>
2018-12-20 13:24:39 +0100

    fix bug

--- public/js/screens/play.js
+++ public/js/screens/play.js
@@ -50,7 +50,7 @@ game.PlayScreen = me.ScreenObject.extend({
     onDestroyEvent: function() {
         me.audio.stopTrack('theme');
         // free the stored instance
-        this.HUD = null;
+		me.game.world.removeChild(this.HUD);
 
         me.input.unbindKey(me.input.KEY.SPACE);
         me.input.unbindPointer(me.input.pointer.LEFT);
